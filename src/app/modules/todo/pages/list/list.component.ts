57d6e55d319334ccf6208daa737aa0935cb8ef96
Parvaneh esmaili <parvanehun@gmail.com>
2025-04-20 21:59:07 -0700

    INIT:Add delete function

--- src/app/modules/todo/pages/list/list.component.ts
+++ src/app/modules/todo/pages/list/list.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NoteModel } from '../../../../-models/note.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [ FormsModule,NgFor],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+notesList:NoteModel [] = [
+  new NoteModel( 1 ,'Test Note' , 'Test Description Note' , new Date()),
+  new NoteModel( 1 ,'Test Note' , 'Test Description Note' , new Date()),
+  new NoteModel( 1 ,'Test Note' , 'Test Description Note' , new Date()),
+]
+
+deleteNote(id: number) {
+  var index = this.notesList.findIndex(x => x.id === id)
+  this.notesList.splice(index,1)
+};
 
 }
